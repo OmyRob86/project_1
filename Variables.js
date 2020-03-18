3e2f4cab8dc4787712e5d2c42d78f8cd0eb221e5
omy86 <harrysytito@gmail.com>
2020-03-18 16:31:20 +0100

    add Variables.js

---
+++ Variables.js
@@ -0,0 +1,42 @@
+console.log('hola amigos !');
+
+var variable1 = 1;
+
+let nbOfScreenColumns  = 2;
+
+nbOfScreenColumns = "two";
+
+variable2 = "ya cambie todo";
+
+const nbOfTry = 3;
+
+
+
+console.log("variable 2 : ", nbOfScreenColumns);
+console.log("variable 3 : ", nbOfTry);
+
+
+//TYPES DE VARIABLES 
+
+const strVariable = "chaine de charactere";
+const strVariableBis = 'chaine de characters entre apostrophes';
+const numberVariable = 3.14;
+const booleanVariable = true;
+const arrayVariable = [
+    "chaines",
+    554,
+    true,
+    [],
+];
+const ObjetVariable = {
+    favBiscuit: "galletas",
+    favNokia: 3310,
+    haveGoodJokes: false,
+    friends: [],
+};
+
+const functionVariable = (arg1, arg2, arg3) => {
+    //traitement de la function
+    return "value to be returned"
+}
+    
