be2a7eae522fb8b5037ff553369b34c9151244da
omy86 <harrysytito@gmail.com>
2020-03-18 16:40:51 +0100

    add Structures.js

---
+++ Structures.js
@@ -0,0 +1,46 @@
+//IF... ELSE
+
+if (numberVariable === 3.14) {
+    console.log("es verdad !");
+} else {
+    console.log("es falso !");
+}
+
+//WHILE
+
+while (numberVariable === 3.24) {
+    console.log("es verdad todavia");
+}
+
+//DO WHILE
+
+do {
+    console.log("es la primera vez, o todavia verdad");
+} while (numberVariable === 3.24);
+
+//FOR... VARIANTE DE .... A .... Y NO DE...
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//SWITCH 
+
+if (numberVariable === "oih") {
+    console.log( "condicion 1 es verdad");
+} else if (strVariable === "kjhs") {
+    console.log("condicion 1 es falsa , pero condicion 2 es verdadera");
+} else {
+    console.log ("toutes les conditions sont fausses. ");
+}
+
+switch(numberVariable) {
+        case "str1": //if (numberVariable === "str1") 
+        console.log("condition 1 es verdad");
+    break;
+    
+
+
+}
+
+//
