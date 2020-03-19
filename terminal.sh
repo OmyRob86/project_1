56c61b784e3dd4a30fbfab3e4f7791f1913248e0
omy86 <harrysytito@gmail.com>
2020-03-19 10:44:41 +0100

    add terminal.sh

---
+++ terminal.sh
@@ -0,0 +1,14 @@
+#DIVERS
+cd <chemin du fichier>  #desplazarse
+node <nom du fichier>   #lanzar un script node
+
+# GIT
+git init                # inicializar un proyecto
+git add <nom du fichier / dossier> #agregar las modoficaciones para el proximo commit
+git commit -m '<mensaje que se agrega al commit>' # guardar las modif precedentes 
+git status              # mostrar el estadod de mi proyecto git en este instante
+git log                 # muestra el record de los commits 
+git restore <nom du fichier / dossier> #restaura lo que quieras del ultimo commit
+git checkout <nom du branche ou hash du commit>  #se desplaza a la cabeza de la lectura de la rama o id commit
+git branch <nom de la branch>
+git branch
